d9ae50274283fedbb974a8edcec554ee5a494ce9
schan <105832330+fullspringwater@users.noreply.github.com>
2022-06-03 10:48:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 from app_home import run_home
-
+from PIL import Image
 def main() :
     st.set_page_config(layout="wide")
-    st.title('Netflix 검색 및 추천')
+    
+    logo = Image.open('data/logo.jpg')
+    col1, col2 = st.columns([2,6])
+    with col1 :
+        st.title('Netflix 검색 및 추천')
+    with col2 :
+        st.image(logo, width=130 )
+    
     run_home() 
-    print(st.__version__)
+
 
 if __name__=='__main__' :
     main()
